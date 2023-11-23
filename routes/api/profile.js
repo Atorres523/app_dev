c1590c2872052a7ff9d66aad5fe2fd679ff64593
jen444x <106173914+jen444x@users.noreply.github.com>
2023-11-22 22:00:11 -0800

    Gets all user profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -106,4 +106,17 @@ router.post(
   }
 );
 
+//@route  GET api/profile
+//@desc   Get all user's profile
+//@access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Sever Error');
+  }
+})
+
 module.exports = router;
