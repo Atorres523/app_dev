d288146a940b9e17b4d556599bc738963fd27401
jen444x <106173914+jen444x@users.noreply.github.com>
2023-12-04 10:48:04 -0800

    added front end login/register functionality. redirects to dashboard

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,11 @@
 import React, {Fragment, useState} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, Navigate} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {setAlert} from '../../actions/alertAction'
 import {register} from '../../actions/auth'
 import PropTypes from 'prop-types'
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -25,6 +25,12 @@ const Register = ({ setAlert, register }) => {
       //console.log(formData)
     }
   }
+
+  if(isAuthenticated) {
+    console.log("worked");
+    return <Navigate to="/dashboard"  />;
+  }
+
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
@@ -83,10 +89,15 @@ const Register = ({ setAlert, register }) => {
 }
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 }
 
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-  null, 
+  mapStateToProps, 
   {setAlert, register}
   )(Register);
