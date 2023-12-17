231b37f88879646ff3433d0707ad663bd5d5be00
CecilioN7 <navarroscecilio@gmail.com>
2023-12-17 12:47:55 -0800

    EditProfile

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,9 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
-import CreateProfile from './components/profile-forms/CreateProfile';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -25,7 +26,7 @@ const App = () => {
     store.dispatch(loadUser());
   }, []);
 
-  return (
+  return ( 
     <Provider store={store}>
       <Router>
         <Fragment>
@@ -36,6 +37,7 @@ const App = () => {
             <Route path='/login' element={<Login />} />
             <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
             <Route path='/create-profile' element={<PrivateRoute><CreateProfile /></PrivateRoute>} />
+            <Route path='/edit-profile' element={<PrivateRoute><EditProfile /></PrivateRoute>} />
           </Routes>
         </Fragment>
       </Router>
