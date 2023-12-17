47361a35f5d3768d24c81e803afd2689ec4f0ca1
CecilioN7 <navarroscecilio@gmail.com>
2023-12-16 17:23:46 -0800

    create your profile

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,14 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-import PrivateRoute from './components/routing/PrivateRoute';
 
 import './App.css';
 
@@ -34,6 +35,7 @@ const App = () => {
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
             <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path='/create-profile' element={<PrivateRoute><CreateProfile /></PrivateRoute>} />
           </Routes>
         </Fragment>
       </Router>
