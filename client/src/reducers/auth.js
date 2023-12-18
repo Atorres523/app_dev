0b325b7b9d2b629b5a79b4ca76689fea6c5d84fb
CecilioN7 <navarroscecilio@gmail.com>
2023-12-18 09:00:05 -0800

    delete user

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
+    ACCOUNT_DELETED,
     LOGOUT
 } from '../actions/types'
 
@@ -40,6 +41,7 @@ export default function authReducer(state = initialState, action) {
         case AUTH_ERROR:
         case LOGIN_FAIL:
         case LOGOUT:
+        case ACCOUNT_DELETED:
             localStorage.removeItem('token');
             return {
                 ...state,
