a27b76a9b0733b964844f9bb6e76562b662f1ac2
Alexis <alexistor583@gmail.com>
2023-12-18 11:53:10 -0800

    fixed small mistakes

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addComment } from "../../actions/post";
@@ -34,4 +34,4 @@ CommentForm.propTypes = {
     addComment: PropTypes.func.isRequired
 }
 
-export default connect(null,{addComment})(CommentItem)
+export default connect(null,{addComment})(CommentForm)
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,40 +1,44 @@
-import React, { Fragment, useEffect } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import Spinner from "../layout/Spinner";
-import { getPosts } from "../../actions/post";
-import { Link } from "react-router-dom";
-import PostItem from "./PostItem";
-import CommentForm from "./post/CommentForm";
-import CommentItem from "./post/CommentItem";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link, useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
+import { getPost } from '../../actions/post';
 
+const Post = ({ getPost, post: { post, loading } }) => {
+  const { id } = useParams();
+  useEffect(() => {
+    getPost(id);
+  }, [getPost, id]);
 
-const Posts = ({ getPosts, post: { post, loading }, match }) => {
-    useEffect(() => {
-        getPosts(match.params.id);
-    }, [getPosts]);
-    
-    return loading || post === null ? <Spinner/> : <Fragment>
-        <Link to = '/posts' className = 'btn'>
-            Back To Posts
-        </Link>
-        <PostItem post={post} showActions={false} />
-        <CommentForm postId={post._id} />
-        <div className="comments">
-            {post.comments.map(comment => (
-                <CommentItem key={comment._id} comment={comment} postId={post._id} />
-            ))} 
-        </div>
-    </Fragment>
-}
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <section className="container">
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-Posts.propTypes = {
-  getPosts: PropTypes.func.isRequired,
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   post: state.post
 });
 
-export default connect(mapStateToProps, { getPosts })(Posts);
+export default connect(mapStateToProps, { getPost })(Post);
