5befbb980c367108ba065f821a482e2cc6a269f8
jen444x <106173914+jen444x@users.noreply.github.com>
2023-12-17 17:09:27 -0800

    Get profile by ID

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -38,6 +38,24 @@ export const getProfiles = () => async dispatch => {
     }
 };
 
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+        
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status:err.response.status }
+        });
+    }
+};
+
+//didnt include github repo bc not using
 
 // Create or update profile
 export const createProfile = (
